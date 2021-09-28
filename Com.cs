dfc24415c3499d3bea4af4519e58cc8affa872c1
Apollo Wayne <waynezhaoyang@gmail.com>
2021-09-28 12:22:22 +0800

    添加项目文件。

---
+++ Com.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ComTransfer
+{
+    public class PCOMM
+    {
+        public const int B50 = 0x00;
+        public const int B75 = 0x01;
+        public const int B110 = 0x02;
+        public const int B134 = 0x03;
+        public const int B150 = 0x04;
+        public const int B300 = 0x05;
+        public const int B600 = 0x06;
+        public const int B1200 = 0x07;
+        public const int B1800 = 0x08;
+        public const int B2400 = 0x09;
+        public const int B4800 = 0x0A;
+        public const int B7200 = 0x0B;
+        public const int B9600 = 0x0C;
+        public const int B19200 = 0x0D;
+        public const int B38400 = 0x0E;
+        public const int B57600 = 0x0F;
+        public const int B115200 = 0x10;
+        public const int B230400 = 0x11;
+        public const int B460800 = 0x12;
+        public const int B921600 = 0x13;
+
+        /*	MODE setting		*/
+        public const int BIT_5 = 0x00;         /* Word length define	*/
+        public const int BIT_6 = 0x01;
+        public const int BIT_7 = 0x02;
+        public const int BIT_8 = 0x03;
+
+        public const int STOP_1 = 0x00;            /* Stop bits define	*/ 
+        public const int STOP_2 = 0x04;
+        public const int P_EVEN = 0x18;            /* Parity define	*/
+        public const int P_ODD = 0x08;
+        public const int P_SPC = 0x38;
+        public const int P_MRK = 0x28;
+        public const int P_NONE = 0x00;
+        /*	MODEM CONTROL settin=g	*/
+        public const int C_DTR = 0x01;
+        public const int C_RTS = 0x02;
+
+        /*	MODEM LINE STATUS	=*/
+        public const int S_CTS = 0x01;
+        public const int S_DSR = 0x02;
+        public const int S_RI = 0x04;
+        public const int S_CD = 0x08;
+
+        /* error code */
+        public const int SIO_OK = 0;
+        public const int SIO_BADPORT = -1;/* no such port or port not opened */
+        public const int SIO_OUTCONTROL = -2;/* can't control the board */
+        public const int SIO_NODATA = -4;/* no data to read or no buffer to write */
+        public const int SIO_OPENFAIL = -5;    /* no such port or port has be opened */
+        public const int SIO_RTS_BY_HW = -6;     /* RTS can't set because H/W flowctrl */
+        public const int SIO_BADPARM = -7;/* bad parameter */
+        public const int SIO_WIN32FAIL = -8;/* call win32 function fail, please call */
+        /* GetLastError to get the error code */
+        public const int SIO_BOARDNOTSUPPORT = -9; /* Does not support this board */
+        public const int SIO_FAIL = -10;/* PComm function run result fail */
+        public const int SIO_ABORTWRITE = -11;/* write has blocked, and user abort write */
+        public const int SIO_WRITETIMEOUT = -12;/* write timeoue has happened */
+
+        public static string GetErrorMessage(int error)
+        {
+            return string.Empty;
+        }
+
+        /* file transfer error code */
+        public const int SIOFT_OK = 0;
+        public const int SIOFT_BADPORT = -1;   /* no such port or port not open */
+        public const int SIOFT_TIMEOUT = -2;/* protocol timeout */
+        public const int SIOFT_ABORT = -3;/* user key abort */
+        public const int SIOFT_FUNC = -4;/* func return abort */
+        public const int SIOFT_FOPEN = -5;/* can not open files */
+        public const int SIOFT_CANABORT = -6;  /* Ymodem CAN signal abort */
+        public const int SIOFT_PROTOCOL = -7;  /* Protocol checking error abort */
+        public const int SIOFT_SKIP = -8;/* Zmodem remote skip this send file */
+        public const int SIOFT_LACKRBUF = -9;  /* Zmodem Recv-Buff size must >= 2K bytes */
+        public const int SIOFT_WIN32FAIL = -10;    /* OS fail */
+        /* GetLastError to get the error code */
+        public const int SIOFT_BOARDNOTSUPPORT = -11;   /* Does not support board */
+
+        public static string GetTransferErrorMessage(int error)
+        {
+            return string.Empty;
+        }
+
+        private const string DLL_NAME = "PCOMM.dll";
+
+        public delegate void pCallback(int port);
+        public delegate int xCallBack(long xmitlen, int buflen, byte[] buf, long flen);
+        public delegate int rCallBack(long recvlen, int buflen, byte[] buf, long flen);
+
+        [DllImport(DLL_NAME)]
+        public static extern int sio_ioctl(int port, int baud, int mode);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_getch(int port);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_read(int port, byte[] buf, int len);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_putch(int port, int term);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_write(int port, byte[] buf, int len);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_flush(int port, int func);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_iqueue(int port);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_oqueue(int port);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_lstatus(int port);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_lctrl(int port, int mode);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_cnt_irq(int port, pCallback cb, int count);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_modem_irq(int port, pCallback cb);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_break_irq(int port, pCallback cb);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_Tx_empty_irq(int port, pCallback cb);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_break(int port, int time);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_break_ex(int port, int time);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_flowctrl(int port, int mode);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_Tx_hold(int port);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_close(int port);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_open(int port);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_getbaud(int port);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_getmode(int port);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_getflow(int port);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_DTR(int port, int mode);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_RTS(int port, int mode);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_baud(int port, long speed);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_data_status(int port);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_term_irq(int port, pCallback cb, char code);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_linput(int port, byte[] buf, int lne, int term);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_putb_x(int port, byte[] buf, int len, int tick);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_putb_x_ex(int port, byte[] buf, int len, int tms);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_view(int port, byte[] buf, int len);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_TxLowWater(int port, int size);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_AbortWrite(int port);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_SetWriteTimeouts(int port, int TotalTimeouts);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_GetWriteTimeouts(int port, ref int TotalTimeouts);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_SetReadTimeouts(int port, int TotalTimeouts, int IntervalTimeouts);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_GetReadTimeouts(int port, ref int TotalTimeouts, ref int IntervalTimeouts);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_AbortRead(int port);
+
+        [DllImport(DLL_NAME)]
+        public static extern int sio_ActXoff(int port);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_ActXon(int port);
+
+        [DllImport(DLL_NAME)]
+        public static extern int sio_FtASCIITx(int port, string fname, xCallBack cb, int key);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_FtASCIIRx(int port, string fname, rCallBack cb, int key, int sec);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_FtXmodemCheckSumTx(int port, string fname, xCallBack cb, int key);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_FtXmodemCheckSumRx(int port, string fname, rCallBack cb, int key);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_FtXmodemCRCTx(int port, string fname, xCallBack cb, int key);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_FtXmodemCRCRx(int port, string fname, rCallBack cb, int key);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_FtXmodem1KCRCTx(int port, string fname, xCallBack cb, int key);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_FtXmodem1KCRCRx(int port, string fname, rCallBack cb, int key);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_FtYmodemTx(int port, string fname, xCallBack cb, int key);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_FtYmodemRx(int port, ref string ffname, int fno, rCallBack cb, int key);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_FtZmodemTx(int port, string fname, xCallBack cb, int key);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_FtZmodemRx(int port, ref string ffname, int fno, rCallBack cb, int key);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_FtKermitTx(int port, string fname, xCallBack cb, int key);
+        [DllImport(DLL_NAME)]
+        public static extern int sio_FtKermitRx(int port, ref string ffname, int fno, rCallBack cb, int key);
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ComTransfer
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void StartSendTask()
+        {
+            PCOMM.xCallBack xCallBack = new PCOMM.xCallBack((long xmitlen, int buflen, byte[] buf, long flen) =>
+            {
+                Console.WriteLine(string.Format("Send:{0}/{1}", xmitlen, flen));
+                return 0;
+            }
+            );
+
+            Task.Factory.StartNew(() =>
+            {
+                string fileName = @"D:\clmq.dll";
+                string tempFolder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+                Directory.CreateDirectory(tempFolder);
+                FileInfo fileInfo = new FileInfo(fileName);
+                string compressedFileName = Path.Combine(tempFolder, fileInfo.Name + ".gz");
+                using (FileStream originalFileStream = new FileStream(fileName, FileMode.Open))
+                {
+                    using (FileStream compressedFileStream = File.Create(compressedFileName))
+                    {
+                        using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                        {
+                            originalFileStream.CopyTo(compressionStream);
+                        }
+                    }
+                }
+                fileName = compressedFileName;
+
+                PCOMM.sio_open(1);
+                PCOMM.sio_ioctl(1, PCOMM.B115200, PCOMM.P_NONE | PCOMM.BIT_8 | PCOMM.STOP_1);
+                PCOMM.sio_flowctrl(1, 0 | 0);
+                PCOMM.sio_DTR(1, 1);
+                PCOMM.sio_RTS(1, 1);
+                PCOMM.sio_flush(1, 2);
+                int result = PCOMM.sio_FtZmodemTx(1, fileName, xCallBack, 27);
+                if (result < 0)
+                {
+                    var a = 1;
+                }
+            });
+        }
+
+        public void StartReceiveTask()
+        {
+            PCOMM.rCallBack rCallBack = new PCOMM.rCallBack((long recvlen, int buflen, byte[] buf, long flen) =>
+            {
+                Console.WriteLine(string.Format("Receive:{0}/{1}", recvlen, flen));
+                return 0;
+            }
+            );
+
+            Task.Factory.StartNew(() =>
+            {
+                string path = "";
+                string tempFolder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+                tempFolder = @"D:\test";
+                Directory.CreateDirectory(tempFolder);
+                Directory.SetCurrentDirectory(tempFolder);
+                int fno = 1;
+                int comport = 2;
+                PCOMM.sio_open(comport);
+                PCOMM.sio_ioctl(comport, PCOMM.B115200, PCOMM.P_NONE | PCOMM.BIT_8 | PCOMM.STOP_1);
+                PCOMM.sio_flowctrl(comport, 0 | 0);
+                PCOMM.sio_DTR(comport, 1);
+                PCOMM.sio_RTS(comport, 1);
+                PCOMM.sio_flush(comport, 2);
+                int result = PCOMM.sio_FtZmodemRx(comport, ref path, fno, rCallBack, 27);
+                if (result < 0)
+                {
+                    var a = 1;
+                }
+
+                FileInfo fileInfo = new FileInfo(path);
+                if (fileInfo.Extension.ToUpper() == ".GZ")
+                {
+                    using (FileStream originalFileStream = new FileStream(path, FileMode.Open))
+                    {
+                        string currentFileName = fileInfo.FullName;
+                        string newFileName = currentFileName.Remove(currentFileName.Length - fileInfo.Extension.Length);
+
+                        using (FileStream decompressedFileStream = File.Create(newFileName))
+                        {
+                            using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                            {
+                                decompressionStream.CopyTo(decompressedFileStream);
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+        private void Button_StartSend_Click(object sender, RoutedEventArgs e)
+        {
+            StartSendTask();
+        }
+
+        private void Button_StartReceive_Click(object sender, RoutedEventArgs e)
+        {
+            StartReceiveTask();
+        }
+    }
+    public class ComPort
+    {
+        public string PortInfo => $"COM{PortID},{BaudRate},{DataBits},{StopBits},{Parity},RTS/CTS:{IsHW},XON/XOFF:{IsSW},RTS:{IsDTR},DTR:{IsRTS}";
+        public string PortStatus => $"";
+        public int PortID = 1;
+        public int BaudRate = 9600;
+        public int DataBits = 8;
+        public int StopBits = 1;
+        public string Parity = "NONE";
+        public bool IsHW = false;
+        public bool IsSW = false;
+        public bool IsDTR = false;
+        public bool IsRTS = false;
+
+        public bool SetPort()
+        {
+            int result;
+            int baudRate, dataBits, stopBits, parity;
+
+            switch (BaudRate)
+            {
+                case 50: baudRate = PCOMM.B50; break;
+                case 75: baudRate = PCOMM.B75; break;
+                case 110: baudRate = PCOMM.B110; break;
+                case 134: baudRate = PCOMM.B134; break;
+                case 150: baudRate = PCOMM.B150; break;
+                case 300: baudRate = PCOMM.B300; break;
+                case 600: baudRate = PCOMM.B600; break;
+                case 1200: baudRate = PCOMM.B1200; break;
+                case 1800: baudRate = PCOMM.B1800; break;
+                case 2400: baudRate = PCOMM.B2400; break;
+                case 4800: baudRate = PCOMM.B4800; break;
+                case 7200: baudRate = PCOMM.B7200; break;
+                case 9600: baudRate = PCOMM.B9600; break;
+                case 19200: baudRate = PCOMM.B19200; break;
+                case 38400: baudRate = PCOMM.B38400; break;
+                case 57600: baudRate = PCOMM.B57600; break;
+                case 115200: baudRate = PCOMM.B115200; break;
+                case 230400: baudRate = PCOMM.B230400; break;
+                case 460800: baudRate = PCOMM.B460800; break;
+                case 921600: baudRate = PCOMM.B921600; break;
+                default: baudRate = PCOMM.B9600; break;
+            }
+
+            switch (DataBits)
+            {
+                case 5:dataBits = PCOMM.BIT_5;break;
+                case 6: dataBits = PCOMM.BIT_6; break;
+                case 7: dataBits = PCOMM.BIT_7; break;
+                case 8: dataBits = PCOMM.BIT_8; break;
+                default:dataBits = PCOMM.BIT_8;break;
+            }
+
+            switch (StopBits)
+            {
+                case 1: stopBits = PCOMM.STOP_1; break;
+                case 2: stopBits = PCOMM.STOP_2; break;
+                default: stopBits = PCOMM.STOP_1; break;
+            }
+
+            switch (Parity?.ToUpper())
+            {
+                case "NONE": parity = PCOMM.P_NONE; break;
+                case "ODD": parity = PCOMM.P_ODD; break;
+                case "EVEN": parity = PCOMM.P_EVEN; break;
+                case "MARK": parity = PCOMM.P_MRK; break;
+                case "SPACE": parity = PCOMM.P_SPC; break;
+                default: parity = PCOMM.P_NONE; break;
+            }
+
+            int port = PortID;
+            int mode = dataBits | stopBits | parity;
+            int hw = IsHW ? 3 : 0;
+            int sw = IsSW ? 12 : 0;
+
+            if((result = PCOMM.sio_ioctl(port, baudRate, mode)) != PCOMM.SIO_OK)
+            {
+                throw new Exception(PCOMM.GetErrorMessage(result));
+            }
+
+            if ((result = PCOMM.sio_flowctrl(port, hw | sw)) != PCOMM.SIO_OK)
+            {
+                throw new Exception(PCOMM.GetErrorMessage(result));
+            }
+
+            if ((result = PCOMM.sio_DTR(port, IsDTR ? 1 : 0)) != PCOMM.SIO_OK)
+            {
+                throw new Exception(PCOMM.GetErrorMessage(result));
+            }
+
+            if (!IsHW)
+            {
+                if ((result = PCOMM.sio_RTS(port, IsDTR ? 1 : 0)) != PCOMM.SIO_OK)
+                {
+                    throw new Exception(PCOMM.GetErrorMessage(result));
+                }
+            }
+
+            return true;
+        }
+    }
+}
