efc0bb04f5676d796252f8beb47e29324d3e968f
Apollo Wayne <shinoring@msn.cn>
2021-10-29 11:27:42 +0800

    Add Unit Test Project

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("caf87f8a-b05c-4b1f-8e22-824db62afe33")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,20 @@
+﻿using ComTransfer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ComTransfer.Tests
+{
+    [TestClass()]
+    public class UnitTest
+    {
+        [TestMethod()]
+        public void SendFileTest()
+        {
+        }
+
+        [TestMethod()]
+        public void SetPortTest()
+        {
+        }
+    }
+}
