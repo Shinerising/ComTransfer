7991dca35914dd8d24af71eda73c4d78ac9b62f0
Apollo Wayne <waynezhaoyang@gmail.com>
2022-09-19 15:09:12 +0800

    Add Comments

--- TaskManager.cs
+++ TaskManager.cs
@@ -10,68 +10,143 @@ using System.Xml.Serialization;
 
 namespace ComTransfer
 {
+    /// <summary>
+    /// 用于记录计划任务信息的基本类
+    /// </summary>
     [Serializable]
     [XmlType(TypeName = "Task")]
     public class TaskNode
     {
+        /// <summary>
+        /// 触发时间
+        /// </summary>
         [XmlAttribute]
         public int Time { get; set; }
 
+        /// <summary>
+        /// 文件修改时间起点
+        /// </summary>
         [XmlAttribute]
         public int FileTime_Head { get; set; }
 
+        /// <summary>
+        /// 文件修改时间终点
+        /// </summary>
         [XmlAttribute]
         public int FileTime_Tail { get; set; }
 
+        /// <summary>
+        /// 目标文件夹
+        /// </summary>
         [XmlAttribute]
         public string Folder { get; set; } = @"C:\";
 
+        /// <summary>
+        /// 文件后缀名
+        /// </summary>
         [XmlAttribute]
         public string Extension { get; set; } = "*.*";
 
+        /// <summary>
+        /// 触发时间的小时数
+        /// </summary>
         [XmlIgnore]
         public int Hour
         {
             get => Time / 60;
             set => Time = value * 60 + Time % 60;
         }
+        /// <summary>
+        /// 触发时间的分钟数
+        /// </summary>
         [XmlIgnore]
         public int Minute
         {
             get => Time % 60;
             set => Time = Time - (Time % 60) + value;
         }
+        /// <summary>
+        /// 实际触发事件
+        /// </summary>
         public DateTime TriggerTime => DateTime.Now.Date + TimeSpan.FromMinutes(Time);
+        /// <summary>
+        /// 实际文件修改时间起点
+        /// </summary>
         public DateTime HeadTime => DateTime.Now - TimeSpan.FromMinutes(FileTime_Head);
+        /// <summary>
+        /// 实际文件修改时间终点
+        /// </summary>
         public DateTime TailTime => DateTime.Now - TimeSpan.FromMinutes(FileTime_Tail);
+        /// <summary>
+        /// 触发时间的文字描述信息
+        /// </summary>
         public string TriggerTimeText => string.Format("{0:00}:{1:00}", Time / 60, Time % 60);
+        /// <summary>
+        /// 获取文本信息
+        /// </summary>
+        /// <returns>文本字符串</returns>
         public override string ToString()
         {
             return string.Format("每天{0}发送{1}文件夹中的{2}文件，修改时间区间为{3:f1}小时至{4:f1}小时", TriggerTimeText, Folder, Extension, FileTime_Head / 60d, FileTime_Tail / 60d);
         }
+        /// <summary>
+        /// 获取浅表复制
+        /// </summary>
+        /// <returns>当前实例的复制</returns>
         public TaskNode Clone()
         {
             return MemberwiseClone() as TaskNode;
         }
     }
+    /// <summary>
+    /// 用于描述发送失败文件的基本类
+    /// </summary>
     [Serializable]
     [XmlType(TypeName = "File")]
     public class FailFileNode
     {
+        /// <summary>
+        /// 文件地址
+        /// </summary>
         [XmlAttribute]
         public string FilePath { get; set; }
+        /// <summary>
+        /// 失败时间
+        /// </summary>
         [XmlAttribute]
         public DateTime FailTime { get; set; }
+        /// <summary>
+        /// 下一次重试时间
+        /// </summary>
         [XmlAttribute]
         public DateTime RetryTime { get; set; }
+        /// <summary>
+        /// 失败计数
+        /// </summary>
         [XmlAttribute]
         public int FailCount { get; set; }
     }
+    /// <summary>
+    /// 计划任务管理器
+    /// </summary>
     public class TaskManager
     {
+        /// <summary>
+        /// 唯一实例
+        /// </summary>
         public static TaskManager Instance = new TaskManager();
+        /// <summary>
+        /// 计划任务列表
+        /// </summary>
         public ObservableCollection<TaskNode> TaskList { get; set; }
+        /// <summary>
+        /// 发送错误文件列表
+        /// </summary>
         public List<FailFileNode> FailFileList { get; set; }
+        /// <summary>
+        /// 读取计划任务列表数据
+        /// </summary>
+        /// <returns>计划任务列表</returns>
         public static List<TaskNode> ReadData()
         {
             try
@@ -88,6 +163,10 @@ namespace ComTransfer
             }
         }
 
+        /// <summary>
+        /// 保存计划任务列表数据
+        /// </summary>
+        /// <param name="list">计划任务列表</param>
         public static void SaveData(List<TaskNode> list)
         {
             try
@@ -102,6 +181,10 @@ namespace ComTransfer
             {
             }
         }
+        /// <summary>
+        /// 读取发送错误文件列表数据
+        /// </summary>
+        /// <returns>发送错误文件列表信息</returns>
         public static List<FailFileNode> ReadFailFileData()
         {
             try
@@ -117,6 +200,10 @@ namespace ComTransfer
                 return new List<FailFileNode>();
             }
         }
+        /// <summary>
+        /// 保存发送错误文件列表数据
+        /// </summary>
+        /// <param name="list">发送错误文件列表信息</param>
         public static void SaveFailFileData(List<FailFileNode> list)
         {
             try
@@ -131,26 +218,59 @@ namespace ComTransfer
             {
             }
         }
+        /// <summary>
+        /// 计划任务文件发送事件参数
+        /// </summary>
         public class FileTaskEventArgs : EventArgs
         {
+            /// <summary>
+            /// 文件地址
+            /// </summary>
             private readonly string file;
+            /// <summary>
+            /// 是否发送成功
+            /// </summary>
             private readonly bool succeed;
+            /// <summary>
+            /// 构造函数
+            /// </summary>
+            /// <param name="filename">文件名称</param>
             public FileTaskEventArgs(string filename)
             {
                 file = filename;
             }
+            /// <summary>
+            /// 构造函数
+            /// </summary>
+            /// <param name="filename">文件名称</param>
+            /// <param name="isSucceed">是否发送成功</param>
             public FileTaskEventArgs(string filename, bool isSucceed)
             {
                 file = filename;
                 succeed = isSucceed;
             }
+            /// <summary>
+            /// 错误消息
+            /// </summary>
             public string Message { get; set; }
 
+            /// <summary>
+            /// 文件地址
+            /// </summary>
             public string File => file;
+            /// <summary>
+            /// 是否发送成功
+            /// </summary>
             public bool IsSucceed => succeed;
         }
         public EventHandler<FileTaskEventArgs> FileTaskHandler;
+        /// <summary>
+        /// 文件发送过程结束处理器
+        /// </summary>
         public EventHandler<FileTaskEventArgs> FileSendedTaskHandler;
+        /// <summary>
+        /// 构造函数
+        /// </summary>
         private TaskManager()
         {
             TaskList = new ObservableCollection<TaskNode>();
@@ -162,6 +282,11 @@ namespace ComTransfer
 
             FileSendedTaskHandler += FileSendedHandler;
         }
+        /// <summary>
+        /// 文件发送过程结束事件响应
+        /// </summary>
+        /// <param name="sender">事件对象</param>
+        /// <param name="e">事件参数</param>
         private void FileSendedHandler(object sender, FileTaskEventArgs e)
         {
             string filename = e.File;
@@ -187,12 +312,18 @@ namespace ComTransfer
             }
         }
 
+        /// <summary>
+        /// 刷新所有计划任务
+        /// </summary>
         public void Refresh()
         {
             TaskList.Clear();
             ReadData().ForEach(item => TaskList.Add(item));
         }
 
+        /// <summary>
+        /// 启动监视任务
+        /// </summary>
         private void Start()
         {
             Task.Factory.StartNew(() =>
@@ -209,8 +340,10 @@ namespace ComTransfer
                         FailFileList.RemoveAt(0);
                     }
 
+                    // 删除失效错误文件项目
                     FailFileList.RemoveAll(item => item.FailCount == -1 || item.FailCount >= 3);
 
+                    // 尝试重新发送失败的文件项目
                     foreach (FailFileNode node in FailFileList)
                     {
                         if (node.FailCount == 0)
@@ -237,6 +370,7 @@ namespace ComTransfer
 
                     List<TaskNode> list = TaskList.ToList();
 
+                    // 检查计划任务信息
                     foreach (TaskNode task in TaskList)
                     {
                         if (task.Folder == null || task.Folder.Length == 0 || task.Extension == null || task.Extension.Length == 0)
