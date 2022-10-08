9c64e1d1551189148ce669eccee0d9bc189c026d
Apollo Wayne <waynezhaoyang@gmail.com>
2022-10-08 10:56:47 +0800

    Add Comments

--- ComTransfer/View/TaskWindow.xaml.cs
+++ ComTransfer/View/TaskWindow.xaml.cs
@@ -7,11 +7,18 @@ using System.Windows.Input;
 namespace ComTransfer
 {
     /// <summary>
-    /// ConfigWindow.xaml 的交互逻辑
+    /// 计划任务修改窗口
     /// </summary>
     public partial class TaskWindow : Window
     {
+        /// <summary>
+        /// 计划任务对象列表
+        /// </summary>
         public ObservableCollection<TaskNode> TaskList { get; set; }
+        /// <summary>
+        /// 构建函数
+        /// </summary>
+        /// <param name="window">父窗口对象</param>
         public TaskWindow(Window window)
         {
             Owner = window;
@@ -23,6 +30,11 @@ namespace ComTransfer
             InitializeComponent();
         }
 
+        /// <summary>
+        /// 窗口关闭事件
+        /// </summary>
+        /// <param name="sender">事件对象</param>
+        /// <param name="e">事件参数</param>
         private void Window_Closing(object sender, CancelEventArgs e)
         {
             FocusManager.SetFocusedElement(this, this);
@@ -31,11 +43,21 @@ namespace ComTransfer
             TaskManager.SaveFailFileData(TaskManager.Instance.FailFileList);
         }
 
+        /// <summary>
+        /// 添加按钮点击事件
+        /// </summary>
+        /// <param name="sender">事件对象</param>
+        /// <param name="e">事件参数</param>
         private void Button_Add_Click(object sender, RoutedEventArgs e)
         {
             TaskList.Add(new TaskNode());
         }
 
+        /// <summary>
+        /// 删除按钮点击事件
+        /// </summary>
+        /// <param name="sender">事件对象</param>
+        /// <param name="e">事件参数</param>
         private void Button_Delete_Click(object sender, RoutedEventArgs e)
         {
             TaskList.Remove(ListBox_Task.SelectedItem as TaskNode);
